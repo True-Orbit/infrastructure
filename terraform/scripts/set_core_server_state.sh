4f8565432b57a3d509211f50cb52d677d18c0608
Kevin <soccerkevint11@gmail.com>
2025-02-21 14:25:05 -0800

    set env vars from old output

--- terraform/scripts/set_core_server_state.sh
+++ terraform/scripts/set_core_server_state.sh
@@ -4,4 +4,10 @@ old_core_server_image_tag=$(terraform output -raw core_server_image_tag)
 echo "TF_VAR_old_core_server_image_tag=$old_core_server_image_tag" >> $GITHUB_ENV
 export TF_VAR_old_core_server_image_tag=$old_core_server_image_tag
 
-echo $old_core_server_image_tag 
+echo $old_core_server_image_tag
+
+old_core_server_secrets=$(terraform output -raw core_server_secrets)
+echo "TF_VAR_old_core_server_secrets=$old_core_server_secrets" >> $GITHUB_ENV
+export TF_VAR_old_core_server_secrets=$old_core_server_secrets
+
+echo $old_core_server_secrets
