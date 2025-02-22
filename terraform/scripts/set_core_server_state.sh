adb9076d428817b722fb4b8ab63550ed42f7435d
Kevin <soccerkevint11@gmail.com>
2025-02-21 16:40:30 -0800

    set fallback for old secrets

--- terraform/scripts/set_core_server_state.sh
+++ terraform/scripts/set_core_server_state.sh
@@ -6,7 +6,7 @@ export TF_VAR_old_core_server_image_tag=$old_core_server_image_tag
 
 echo $old_core_server_image_tag
 
-old_core_server_secrets=$(terraform output -raw core_server_secrets)
+old_core_server_secrets=$(terraform output -raw core_server_secrets 2>/dev/null || echo [])
 echo "TF_VAR_old_core_server_secrets=$old_core_server_secrets" >> $GITHUB_ENV
 export TF_VAR_old_core_server_secrets=$old_core_server_secrets
 
--- terraform/scripts/set_web_service_state.sh
+++ terraform/scripts/set_web_service_state.sh
@@ -6,7 +6,7 @@ export TF_VAR_old_web_image_tag=$old_web_image_tag
 
 echo $old_web_image_tag
 
-old_web_service_secrets=$(terraform output -raw web_service_secrets)
+old_web_service_secrets=$(terraform output -raw web_service_secrets 2>/dev/null || echo [])
 echo "TF_VAR_old_web_service_secrets=$old_web_service_secrets" >> $GITHUB_ENV
 export TF_VAR_old_web_service_secrets=$old_web_service_secrets
 
