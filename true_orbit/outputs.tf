7bd5e6d6aa420564b6894e07ea20a4e8b58dfafd
Kevin <soccerkevint11@gmail.com>
2025-02-24 11:16:39 -0800

    format

--- true_orbit/outputs.tf
+++ true_orbit/outputs.tf
@@ -3,7 +3,7 @@ output "web_image_tag" {
 }
 
 output "web_service_secrets" {
-  value = local.web_service_secrets
+  value     = local.web_service_secrets
   sensitive = true
 }
 
@@ -12,7 +12,7 @@ output "core_server_image_tag" {
 }
 
 output "core_server_secrets" {
-  value = local.core_server_secrets
+  value     = local.core_server_secrets
   sensitive = true
 }
 
