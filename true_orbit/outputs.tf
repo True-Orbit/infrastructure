5b67269582db3f9995ad51ccb85761f729ee8481
Kevin <soccerkevint11@gmail.com>
2025-02-24 10:27:40 -0800

    format

--- true_orbit/outputs.tf
+++ true_orbit/outputs.tf
@@ -3,8 +3,8 @@ output "web_image_tag" {
 }
 
 output "web_service_secrets" {
-  value = local.web_service_secrets
-  # sensitive = true
+  value     = local.web_service_secrets
+  sensitive = true
 }
 
 output "core_server_image_tag" {
@@ -12,14 +12,6 @@ output "core_server_image_tag" {
 }
 
 output "core_server_secrets" {
-  value = local.core_server_secrets
-  # sensitive = true
-}
-
-output "vpc_id" {
-  value = module.foundation.vpc_id
-}
-
-output "public_subnet_id" {
-  value = module.foundation.public_subnet_a_id
+  value     = local.core_server_secrets
+  sensitive = true
 }
