b413816b205b387386e8a982e6aec5ae4b9c8676
Kevin <soccerkevint11@gmail.com>
2025-02-24 10:26:46 -0800

    terraform apply ec2 instance works

--- true_orbit/outputs.tf
+++ true_orbit/outputs.tf
@@ -4,7 +4,7 @@ output "web_image_tag" {
 
 output "web_service_secrets" {
   value = local.web_service_secrets
-  sensitive = true
+  # sensitive = true
 }
 
 output "core_server_image_tag" {
@@ -13,7 +13,7 @@ output "core_server_image_tag" {
 
 output "core_server_secrets" {
   value = local.core_server_secrets
-  sensitive = true
+  # sensitive = true
 }
 
 output "vpc_id" {
