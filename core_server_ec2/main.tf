969edbd92bffe3469b0b8a98979279dbca98f938
Kevin <soccerkevint11@gmail.com>
2025-02-24 19:55:31 -0800

    fix interpolation

--- core_server_ec2/main.tf
+++ core_server_ec2/main.tf
@@ -101,10 +101,10 @@ resource "aws_instance" "core_server" {
     %{for secret in local.core_server_secrets~}
     export ${secret.name}=$(aws secretsmanager get-secret-value --secret-id ${secret.valueFrom} --query SecretString --output text --region ${var.region})
     echo "Loaded secret ${secret.name}"
-    ENV="$ENV --env ${secret.name}=\$${secret.name}"
+    ENV="$ENV --env ${secret.name}=\$\${secret.name}"
     %{endfor~}
 
-    CMD="$CMD npm unpackSecrets"
+    CMD="npm unpackSecrets"
     
     if [ "${var.migrate}" = "true" ]; then
       CMD="$CMD && npm migrate"
